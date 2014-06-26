90352da0e72784cee35d7a3fc60d50a0ac79f7f6
Julia Rietveld <julia_rietveld@yahoo.com>
2014-06-26 09:30:20 +0200

    serivces front and back

--- public/js/services.js
+++ public/js/services.js
@@ -1,40 +1,37 @@
 app.factory("Article", function($resource, $http) {
   var resource = $resource("/api/articles/:id", { id: "@_id" },
     {
-      'create':  { method: 'POST' },
       'index':   { method: 'GET', isArray: true },
-      'show':    { method: 'GET', isArray: false },
-      'update':  { method: 'PUT' },
-      'destroy': { method: 'DELETE' }
+      'show':    { method: 'GET', isArray: false }
+
     }
   );
 
   return resource;
 });
 
-
-app.factory("ArticleCategory", function($resource, $http) {
-  var resource = $resource("/api/articles/category/:id", { id: "@_id" },
-    {
-      'index':   { method: 'GET', isArray: true },
-    }
-  );
-
-  return resource;
-});
 
 app.factory("Category", function($resource, $http) {
   var resource = $resource("/api/categories/:id", { id: "@_id" },
     {
-      'create':  { method: 'POST' },
       'index':   { method: 'GET', isArray: true },
-      'show':    { method: 'GET', isArray: false },
-      'update':  { method: 'PUT' },
-      'destroy': { method: 'DELETE' }
+      'show':    { method: 'GET', isArray: false }
+
     }
   );
 
   return resource;
 });
 
+appAdmin.factory("User", function($resource, $http) {
+    var resource = $resource("/api/user/:id", { id: "@_id" },
+        {
 
+            'index':   { method: 'GET', isArray: true },
+            'show':    { method: 'GET', isArray: false }
+
+        }
+    );
+
+    return resource;
+});
--- public/js/servicesAdmin.js
+++ public/js/servicesAdmin.js
@@ -13,16 +13,6 @@ appAdmin.factory("Article", function($resource, $http) {
 });
 
 
-appAdmin.factory("ArticleCategory", function($resource, $http) {
-  var resource = $resource("/api/articles/category/:id", { id: "@_id" },
-    {
-      'index':   { method: 'GET', isArray: true },
-    }
-  );
-
-  return resource;
-});
-
 appAdmin.factory("Category", function($resource, $http) {
   var resource = $resource("/api/categories/:id", { id: "@_id" },
     {
@@ -35,6 +25,20 @@ appAdmin.factory("Category", function($resource, $http) {
   );
 
   return resource;
+});
+
+appAdmin.factory("User", function($resource, $http) {
+    var resource = $resource("/api/user/:id", { id: "@_id" },
+        {
+            'create':  { method: 'POST' },
+            'index':   { method: 'GET', isArray: true },
+            'show':    { method: 'GET', isArray: false },
+            'update':  { method: 'PUT' },
+            'destroy': { method: 'DELETE' }
+        }
+    );
+
+    return resource;
 });
 
 
