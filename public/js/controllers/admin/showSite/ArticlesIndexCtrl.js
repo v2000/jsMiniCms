75a44083dd0fc2eaa17b06997b988310cf98f7d0
v2000 <vrpr2008@gmail.com>
2014-07-01 13:13:55 +0200

    delete folder

--- public/js/controllers/admin/showSite/ArticlesIndexCtrl.js
+++
@@ -1,5 +0,0 @@
-appAdmin.controller("ArticlesIndexCtrl", function($scope, $location, Article, Category) {
-  $scope.articles = Article.index();
-  $scope.categories = Category.index();
-
-});
--- public/js/controllers/admin/showSite/ArticlesShowCtrl.js
+++
@@ -1,4 +0,0 @@
-appAdmin.controller("ArticlesShowCtrl", function($scope, $routeParams, Article) {
-  $scope.article = Article.show({ id: $routeParams.id });
-   //concole.log("$scope.article",$scope.article);
-});
