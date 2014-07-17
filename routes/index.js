3fd90731758c86c167068d3d94af6d8b217dab83
v2000 <vrpr2008@gmail.com>
2014-07-17 14:40:37 +0200

    changes in routes

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ module.exports = function(app, passport) {
  // process the login form
   app.post('/login', passport.authenticate('local-login', {
     //successRedirect : 'partials/admin/templates/articles/listview', // redirect to the secure profile section
-    successRedirect : 'partials/site/articlesListView', // redirect to the secure profile section
+    successRedirect : '/admin/articles/listview', // redirect to the secure profile section
     failureRedirect : '/login', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
   }));
@@ -35,7 +35,7 @@ module.exports = function(app, passport) {
 
   // process the signup form
   app.post('/signup', passport.authenticate('local-signup', {
-    successRedirect : 'partials/admin/templates/articles/listview', // redirect to the secure profile section
+    successRedirect : '/admin/articles/listview', // redirect to the secure profile section
     failureRedirect : '/signup', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
   }));
