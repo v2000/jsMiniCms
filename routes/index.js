10f3d5bd25d9f9a2af976f595484f6a3c7d47f85
v2000 <vrpr2008@gmail.com>
2014-07-17 13:57:15 +0200

    small changes

--- routes/index.js
+++ routes/index.js
@@ -19,8 +19,8 @@ module.exports = function(app, passport) {
  // process the login form
   app.post('/login', passport.authenticate('local-login', {
     //successRedirect : 'partials/admin/templates/articles/listview', // redirect to the secure profile section
-    successRedirect : 'partials/admin/templates/articles/listview', // redirect to the secure profile section
-    failureRedirect : 'partials/admin/templates/login', // redirect back to the signup page if there is an error
+    successRedirect : 'partials/site/articlesListView', // redirect to the secure profile section
+    failureRedirect : '/login', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
   }));
 
@@ -36,7 +36,7 @@ module.exports = function(app, passport) {
   // process the signup form
   app.post('/signup', passport.authenticate('local-signup', {
     successRedirect : 'partials/admin/templates/articles/listview', // redirect to the secure profile section
-    failureRedirect : 'partials/admin/templates/signup', // redirect back to the signup page if there is an error
+    failureRedirect : '/signup', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
   }));
 
