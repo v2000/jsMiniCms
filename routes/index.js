615804fa49ff2c55bb386489c49250a502ef4ab1
v2000 <vrpr2008@gmail.com>
2014-07-19 12:45:12 +0200

    changes

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,8 @@ module.exports = function(app, passport) {
 
  // process the login form
   app.post('/admin/login', passport.authenticate('local-login', {
-    //successRedirect : 'partials/admin/templates/articles/listview', // redirect to the secure profile section
+    redirect:'/admin',
+
     successRedirect : '/admin/profile', // redirect to the secure profile section
     failureRedirect : '/admin/login', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
